30f3bb26b69db1a13ef237ba81ea68676d1c4124
Yan Qian <qianyan@thoughtworks.com>
2018-04-24 02:52:35 +0800

    match different networks

--- src/p2b.js
+++ src/p2b.js
@@ -13,7 +13,15 @@ class P2B {
   }
 
   to = async (address, value) => {
-    let exist = await this.contract.deployed()
+    let currentNetwork = this.web3.currentProvider.publicConfigStore._state.networkVersion
+    let exist
+    if(currentNetwork === "1") { // mainnet
+      exist = await this.contract.at('0x78ae0a977d3e57ce52423a1d18fc6a79774ef611')
+    } else if (currentNetwork === "3") {  // ropsten
+      exist = await this.contract.at('0x3720cbd9272473444b96787d686748792c6b9b88')
+    } else {
+      exist = await this.contract.deployed()
+    }
     let owner = await this.myAddress()
     return await exist.transfer(address, value * 100, {from: owner})
   }
