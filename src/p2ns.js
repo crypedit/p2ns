cbc295e57f771a9f806bd887650938ebab94ee44
Fan Jiang <fanjiang@thoughtworks.com>
2018-04-25 12:29:36 +0800

    change contract address

--- src/p2ns.js
+++ src/p2ns.js
@@ -16,11 +16,11 @@ class P2NS {
     let currentNetwork = this.web3.currentProvider.publicConfigStore._state.networkVersion
     let exist
     if(currentNetwork === "1") { // mainnet
-      exist = await this.contract.at('0x78ae0a977d3e57ce52423a1d18fc6a79774ef611')
+      exist = await this.contract.at('0x0')
     } else if (currentNetwork === "3") {  // ropsten
-      exist = await this.contract.at('0x3720cbd9272473444b96787d686748792c6b9b88')
+      exist = await this.contract.at('0x0')
     } else if (currentNetwork === "4") { // rinkeby
-      exist = await this.contract.at('0x60b0dff7e53165e9142872f5d631070517db9621')
+      exist = await this.contract.at('0xe9880eedd4d44ec0b25f3a362af5b8dfd2754c62')
     } else {
       exist = await this.contract.deployed()
     }
