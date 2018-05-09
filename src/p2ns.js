35f012e0e1eb99c8c0f7e50c9de2f60efa4fcc4a
Fan Jiang <fanjiang@thoughtworks.com>
2018-05-09 13:13:26 +0800

    use ropsten by default when no metamask

--- src/p2ns.js
+++ src/p2ns.js
@@ -13,16 +13,20 @@ class P2NS {
   }
 
   async p2nsContract() {
-    let currentNetwork = this.web3.currentProvider.publicConfigStore._state.networkVersion
     let exist
-    if(currentNetwork === "1") { // mainnet
-      exist = await this.contract.at('0x0')
-    } else if (currentNetwork === "3") {  // ropsten
-      exist = await this.contract.at('0x809b72f93776771128723ba54914920ba1341506')
-    } else if (currentNetwork === "4") { // rinkeby
-      exist = await this.contract.at('0xe9880eedd4d44ec0b25f3a362af5b8dfd2754c62')
-    } else {
-      exist = await this.contract.deployed()
+    try {
+      let currentNetwork = this.web3.currentProvider.publicConfigStore._state.networkVersion
+      if(currentNetwork === "1") { // mainnet
+        exist = await this.contract.at('0x0')
+      } else if (currentNetwork === "3") {  // ropsten
+        exist = await this.contract.at('0x809b72f93776771128723ba54914920ba1341506')
+      } else if (currentNetwork === "4") { // rinkeby
+        exist = await this.contract.at('0xe9880eedd4d44ec0b25f3a362af5b8dfd2754c62')
+      } else {
+        exist = await this.contract.deployed()
+      }
+    } catch(e) { // ropsten by default
+        exist = await this.contract.at('0x809b72f93776771128723ba54914920ba1341506')
     }
     return exist
   }
